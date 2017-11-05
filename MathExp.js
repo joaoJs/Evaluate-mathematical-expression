955736c7cfde64c0c3e733ddd78de1508f5cfeeb
Joao Campos <j.campos4g@gmail.com>
2017-11-05 15:47:52 -0500

    in progress, working on multiplication

--- MathExp.js
+++ MathExp.js
@@ -1,15 +1,33 @@
 var calc = function (exp) {
-  // detect for parentheses
-  //getOriginal(exp);
-  detectParentheses(exp);
-};
+  // exp = detectParentheses(exp);
+  // console.log("In the main function --> ", exp);
+  // // exp2 = detectParentheses(exp);
+  // // console.log("2nd --> ", exp2);
+  // // exp3 = detectParentheses(exp2);
+  // // console.log(exp3);
+  // // exp4 = detectParentheses(exp3);
+  // // console.log(exp4);
 
-var getOriginal = function(exp) {
-  var obj = {};
+  //make a copy
+
+  const expCopy = exp;
+  let currentExp = exp;
+  while (exp.includes("(")) {
+    console.log("\n");
+    console.log("EXP --> ", exp);
+    currentExp = exp;
+    exp = detectParentheses(exp);
+  }
+  console.log("");
+  console.log("Res");
+  console.log(exp + " ----- " + currentExp);
+  console.log(calcSections(exp));
 
 }
 
 var detectParentheses = function(exp) {
+  console.log("\n")
+  console.log("Expression in detectParentheses", exp);
   var content = [];
   var start = [];
   var end = [];
@@ -31,28 +49,93 @@ var detectParentheses = function(exp) {
   }
   sections = start.length;
   for (i = 0; i < sections; i++) {
-    content.push([exp.slice(start[i] + 1, end[i])],[start[i], end[i]]);
+    content.push(exp.slice(start[i] + 1, end[i]));
   }
-  console.log(start, end);
-  console.log("Content --> ", content);
-  content.forEach((c,i) => {
-    if (i % 2 === 0) {
-      if (c[0].includes("(")) {
-        detectParentheses(c[0]);
-      } else {
-        // replace original using regex
-        calcSections(c[0]);
+  //console.log("Content --> ", content);
+  for (let i = 0; i < content.length; i++) {
+      const c = content[i];
+      // if (c.includes("(")) {
+      //   detectParentheses(c);
+      // } else {
+      //   // replace original
+      //   exp2 = exp.replace("(" + c + ")", calcSections(c));
+      //   console.log("exp2 --> ", exp2);
+      //   if (exp2.includes("(")) {
+      //     console.log("HERE! --> ", exp2);
+      //     var test = detectParentheses(exp2);
+      //   } else {
+      //     console.log("REs -->", calcSections(exp2));
+      //     return calcSections(exp2);
+      //   }
+      // }
+      //console.log("Cs -->  " , c);
+      if (!c.includes("(")) {
+        // replace original
+        exp = exp.replace("(" + c + ")", calcSections(c));
+        //console.log("exp --> ", exp);
+        return exp;
       }
+  }
+  for (let i = 0; i < content.length; i++) {
+    const c = content[i];
+    if (c.includes("(")) {
+      //console.log(c);
+      return c;
+      // exp = exp.replace("(" + c + ")", detectParentheses(c));
+      // return exp;
     }
-  });
+  }
 }
 
 var calcSections = function(section, indexes ) {
-  console.log("Section --> ", section);
+  //console.log("Section --> ", section);
+  let hasMult = false;
+  let hasDiv = false;
+  if (section.includes("*")) {
+    hasMult = true;
+  } else if (section.includes("/")) {
+    hasDiv = true;
+  }
   var numbers = [];
   var operations = [];
   var splitted = section.split(" ");
   console.log("Splitted", splitted);
+
+
+  // make function for if has mult
+
+  let indMult = [];
+  let indDiv = [];
+  let multRes = 1;
+  let divRes = [];
+  let nextOp;
+  let nextOpInd;
+  if (hasMult) {
+    splitted.forEach((n,i) => {
+      if (n === "*") {
+        indMult.push(i - 1);
+        multRes = splitted[i-1];
+        multRes *= splitted[i+1];
+        if (splitted[i+2] && splitted[i+2] === "*") {
+          while(splitted[i+2] === "*") {
+            i += 2;
+            multRes *= i+1;
+          }
+        }
+        if (splitted[i+2] && splitted[i+2] !== "*") {
+          nextOp = splitted[i+2];
+          nextOpInd = splitted[i+2];
+          splitted[indMult] = multRes;
+        } else if (!splitted[i+2]) {
+          splitted[indMult] = multRes;
+          console.log(splitted.slice(0, indMult[0] + 1));
+          splitted = splitted.slice(0, indMult[0] + 1);
+        }
+      }
+    });
+  }
+
+
   splitted.forEach(e => {
     if ("+-/*".includes(e)) {
       operations.push(e);
@@ -60,6 +143,10 @@ var calcSections = function(section, indexes ) {
       numbers.push(Number(e));
     }
   });
+
+  console.log("");
+  console.log("---->>> Splitted after mult");
+  console.log(splitted);
 
   var res = numbers.reduce((a,b) => {
     var i = 0;
@@ -69,18 +156,19 @@ var calcSections = function(section, indexes ) {
     } else if (operations[i] === "-") {
       i++;
       return a - b;
-    } else if (operations[i] === "*") {
-      i++;
-      return a * b;
-    } else if (operations[i] === "/") {
-      i++;
-      return a / b;
     }
-  }, 0);
-  console.log(res);
+    // else if (operations[i] === "*") {
+    //   i++;
+    //   return a * b;
+    // } else if (operations[i] === "/") {
+    //   i++;
+    //   return a / b;
+    // }
+  });
+  return res;
 }
 
 
-const expression = "(2 / (2 + 3.33) * 4) - -6 -(7 + 6)";
+const expression = "(2 / (2 + -(56 - 55) * 3.33) * 4) - -6 -(7 + 6)";
 
 calc(expression);
